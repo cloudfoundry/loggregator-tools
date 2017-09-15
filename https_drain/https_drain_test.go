7d455eea4e1e0d8f1ead8bb81219b68c402d97e2
Brady Love <blove@pivotal.io>
2017-09-15 10:18:53 -0600

    Backfill https_drain tests
    [#150815626]
    
    Signed-off-by: Johanna Smith <josmith@pivotal.io>

---
+++ https_drain/https_drain_test.go
@@ -0,0 +1,124 @@
+package main_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os/exec"
+	"strings"
+	"time"
+
+	"code.cloudfoundry.org/rfc5424"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("HttpsDrain", func() {
+	var (
+		port = 1354
+	)
+
+	It("writes out the body of the request it receives", func() {
+		_, kill, stdout, _ := startHTTPSDrain(port, "")
+		defer kill()
+
+		body := "to check body output"
+		_ = writeLog(port, body)
+
+		Expect(stdout.Contents()).To(ContainSubstring(body))
+	})
+
+	It("returns a 400 when no body is given", func() {
+		_, kill, _, _ := startHTTPSDrain(port, "")
+		defer kill()
+
+		resp := writeLog(port, "")
+
+		Expect(resp.StatusCode).To(Equal(http.StatusBadRequest))
+	})
+
+	Context("with a COUNTER_URL", func() {
+		It("reports log counts to the COUNTER_URL", func() {
+			reqs := make(chan *http.Request, 100)
+			bodies := make(chan []byte, 100)
+			ts := httptest.NewServer(
+				http.HandlerFunc(
+					func(w http.ResponseWriter, r *http.Request) {
+						reqs <- r
+
+						body, err := ioutil.ReadAll(r.Body)
+						if err != nil {
+							panic(err)
+						}
+
+						bodies <- body
+					},
+				),
+			)
+			defer ts.Close()
+
+			// 1s before it posts
+			_, kill, _, _ := startHTTPSDrain(port, ts.URL)
+			defer kill()
+
+			var req *http.Request
+			Eventually(reqs).Should(Receive(&req))
+			Expect(req.Method).To(Equal("POST"))
+			Expect(req.URL.RequestURI()).To(Equal("/set/"))
+
+			rfcLog := rfc5424.Message{
+				Priority:  rfc5424.Emergency,
+				Timestamp: time.Now(),
+				Hostname:  "some-host",
+				AppName:   "some-app",
+				ProcessID: "procID",
+				MessageID: "msgID",
+				Message:   []byte(`{"id":"some-id", "msgCount":1}`),
+			}
+
+			data, err := rfcLog.MarshalBinary()
+			Expect(err).ToNot(HaveOccurred())
+
+			writeLog(port, string(data))
+			Eventually(bodies).Should(Receive(MatchJSON(`[{"id":"some-id", "msgCount":1, "primeCount":0}]`)))
+		})
+	})
+})
+
+func startHTTPSDrain(port int, counterUrl string) (*gexec.Session, func(), *gbytes.Buffer, *gbytes.Buffer) {
+	path, err := gexec.Build("code.cloudfoundry.org/loggregator-tools/https_drain")
+	Expect(err).ToNot(HaveOccurred())
+
+	cmd := exec.Command(path)
+	cmd.Env = []string{
+		fmt.Sprintf("PORT=%d", port),
+		fmt.Sprintf("COUNTER_URL=%s", counterUrl),
+		"INTERVAL=100ms",
+	}
+
+	stdout, stderr := gbytes.NewBuffer(), gbytes.NewBuffer()
+	session, err := gexec.Start(cmd, stdout, stderr)
+	Expect(err).ToNot(HaveOccurred())
+
+	return session, func() { session.Kill().Wait() }, stdout, stderr
+}
+
+func writeLog(port int, body string) *http.Response {
+	// log := `107 <7>1 2016-02-28T09:57:10.804642398-05:00 myhostname someapp - - [foo@1234 Revision="1.2.3.4"] Hello, World!`
+	var response *http.Response
+	Eventually(func() error {
+		var err error
+		response, err = http.Post(
+			fmt.Sprintf("http://localhost:%d", port),
+			"text/plain",
+			strings.NewReader(body),
+		)
+		return err
+	}).ShouldNot(HaveOccurred())
+
+	return response
+}
---
+++ https_drain/main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	"code.cloudfoundry.org/rfc5424"
+)
+
+func main() {
+	envInterval := os.Getenv("INTERVAL")
+	interval, err := time.ParseDuration(envInterval)
+	if err != nil {
+		if envInterval != "" {
+			log.Fatal(err)
+		}
+
+		interval = time.Second
+	}
+
+	handler := NewSyslog()
+	if os.Getenv("COUNTER_URL") != "" {
+		go handler.reportCount(interval)
+	}
+
+	http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), handler)
+}
+
+type Counter struct {
+	primeCount uint64
+	msgCount   uint64
+}
+
+// populate handler with json from log message
+type Handler struct {
+	mu       sync.Mutex
+	counters map[string]*Counter
+}
+
+func NewSyslog() *Handler {
+	return &Handler{
+		counters: make(map[string]*Counter),
+	}
+}
+
+func (h *Handler) reportCount(interval time.Duration) {
+	url := os.Getenv("COUNTER_URL") + "/set/"
+	if url == "" {
+		log.Fatalf("Missing COUNTER_URL environment variable")
+	}
+
+	for range time.Tick(interval) {
+		payload, err := json.Marshal(h.fetchCounters())
+		if err != nil {
+			log.Panicf("Failed to marshal counters: %s", err)
+		}
+
+		log.Printf("Posting %s", string(payload))
+		resp, err := http.Post(url, "application/json", bytes.NewReader(payload))
+
+		if err != nil {
+			log.Printf("Failed to write count: %s", err)
+			continue
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			log.Printf("Failed to write count: expected 200 got %d", resp.StatusCode)
+		}
+	}
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		log.Printf("Failed to read body: %s", err)
+		return
+	}
+
+	if len(body) < 1 {
+		w.WriteHeader(http.StatusBadRequest)
+		log.Print("Empty body")
+		return
+	}
+
+	// NOTE: This is not a log line. We want the messages printed to STDOUT.
+	fmt.Printf("Received: %s", string(body))
+
+	msg := rfc5424.Message{}
+	err = msg.UnmarshalBinary(body)
+	if err != nil {
+		log.Printf("Failed to unmarshal (via RFC-5424) message: %s", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// NOTE: Only one of "primeCount" or "msgCount" will be set.
+	var msgCounts messageCount
+	err = json.Unmarshal(msg.Message, &msgCounts)
+	if err != nil {
+		log.Printf("Failed to unmarshal (via JSON) message (%s): %s", string(msg.Message), err)
+		return
+	}
+
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	c, ok := h.counters[msgCounts.ID]
+	if !ok {
+		c = new(Counter)
+		h.counters[msgCounts.ID] = c
+	}
+
+	c.primeCount += msgCounts.PrimeCount
+	c.msgCount += msgCounts.MsgCount
+
+	return
+}
+
+type messageCount struct {
+	ID         string `json:"id"`
+	PrimeCount uint64 `json:"primeCount"`
+	MsgCount   uint64 `json:"msgCount"`
+}
+
+// fetchCounters extracts the current counter list and returns a slice of
+// results in a thread safe manner.
+func (h *Handler) fetchCounters() []messageCount {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	var results []messageCount
+	for k, v := range h.counters {
+		results = append(results, messageCount{
+			ID:         k,
+			PrimeCount: v.primeCount,
+			MsgCount:   v.msgCount,
+		})
+	}
+	return results
+}
