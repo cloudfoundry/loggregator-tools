badb61bdedff87e5b6614c25b7ac98acf2890e6b
Jason Keene <jkeene@pivotal.io>
2018-04-11 10:10:18 -0600

    Add syslog egress visitor func
    [#156043714]
    
    Signed-off-by: Johanna Smith <josmith@pivotal.io>

---
+++ log-cache-forwarders/cmd/syslog/internal/egress/visitor.go
@@ -0,0 +1,36 @@
+package egress
+
+import (
+	"log"
+
+	logcache "code.cloudfoundry.org/go-log-cache"
+
+	"code.cloudfoundry.org/go-loggregator/rpc/loggregator_v2"
+)
+
+type metrics interface {
+	NewCounter(string) func(uint64)
+}
+
+func NewVisitor(w Writer, metrics metrics) logcache.Visitor {
+	ingress := metrics.NewCounter("Ingress")
+	egress := metrics.NewCounter("Egress")
+	dropped := metrics.NewCounter("Dropped")
+
+	return func(envs []*loggregator_v2.Envelope) bool {
+		var droppedCount int
+		for _, e := range envs {
+			if w.Write(e) != nil {
+				droppedCount++
+			}
+		}
+
+		sentCount := len(envs) - droppedCount
+		ingress(uint64(len(envs)))
+		dropped(uint64(droppedCount))
+		egress(uint64(sentCount))
+
+		log.Printf("Wrote %d envelopes to syslog!", sentCount)
+		return true
+	}
+}
