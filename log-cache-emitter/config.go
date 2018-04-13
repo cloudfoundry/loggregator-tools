60280f238b1f8df120eadd6f971fc34392346e65
Warren Fernandes <wfernandes@pivotal.io>
2018-04-12 18:18:13 -0600

    Add tool for emitting into log-cache
    Signed-off-by: Jason Keene <jkeene@pivotal.io>

---
+++ log-cache-emitter/config.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"io/ioutil"
+	"log"
+
+	envstruct "code.cloudfoundry.org/go-envstruct"
+	"google.golang.org/grpc/credentials"
+)
+
+// Config is the configuration for a LogCache.
+type Config struct {
+	LogCacheAddr string `env:"LOG_CACHE_ADDR, required"`
+	LogCacheTLS  TLS
+}
+
+type TLS struct {
+	CAPath   string `env:"CA_PATH,   required"`
+	CertPath string `env:"CERT_PATH, required"`
+	KeyPath  string `env:"KEY_PATH,  required"`
+}
+
+func (t TLS) Credentials(cn string) credentials.TransportCredentials {
+	creds, err := NewTLSCredentials(t.CAPath, t.CertPath, t.KeyPath, cn)
+	if err != nil {
+		log.Fatalf("failed to load TLS config: %s", err)
+	}
+
+	return creds
+}
+
+func NewTLSCredentials(
+	caPath string,
+	certPath string,
+	keyPath string,
+	cn string,
+) (credentials.TransportCredentials, error) {
+	cfg, err := NewTLSConfig(caPath, certPath, keyPath, cn)
+	if err != nil {
+		return nil, err
+	}
+
+	return credentials.NewTLS(cfg), nil
+}
+
+func NewTLSConfig(caPath, certPath, keyPath, cn string) (*tls.Config, error) {
+	cert, err := tls.LoadX509KeyPair(certPath, keyPath)
+	if err != nil {
+		return nil, err
+	}
+
+	tlsConfig := &tls.Config{
+		ServerName:         cn,
+		Certificates:       []tls.Certificate{cert},
+		InsecureSkipVerify: false,
+	}
+
+	caCertBytes, err := ioutil.ReadFile(caPath)
+	if err != nil {
+		return nil, err
+	}
+
+	caCertPool := x509.NewCertPool()
+	if ok := caCertPool.AppendCertsFromPEM(caCertBytes); !ok {
+		return nil, errors.New("cannot parse ca cert")
+	}
+
+	tlsConfig.RootCAs = caCertPool
+
+	return tlsConfig, nil
+}
+
+// LoadConfig creates Config object from environment variables
+func LoadConfig() (*Config, error) {
+	c := Config{
+		LogCacheAddr: ":8080",
+	}
+
+	if err := envstruct.Load(&c); err != nil {
+		return nil, err
+	}
+
+	return &c, nil
+}
---
+++ log-cache-emitter/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"log"
+	_ "net/http/pprof"
+	"time"
+
+	envstruct "code.cloudfoundry.org/go-envstruct"
+	"code.cloudfoundry.org/go-log-cache/rpc/logcache_v1"
+	"code.cloudfoundry.org/go-loggregator/rpc/loggregator_v2"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	log.Print("Starting LogCache Nozzle...")
+	defer log.Print("Closing LogCache Nozzle.")
+
+	cfg, err := LoadConfig()
+	if err != nil {
+		log.Fatalf("invalid configuration: %s", err)
+	}
+
+	envstruct.WriteReport(cfg)
+
+	conn, err := grpc.Dial(cfg.LogCacheAddr, grpc.WithTransportCredentials(
+		cfg.LogCacheTLS.Credentials("log-cache"),
+	))
+	if err != nil {
+		log.Fatalf("failed to dial %s: %s", cfg.LogCacheAddr, err)
+	}
+	client := logcache_v1.NewIngressClient(conn)
+
+	for {
+		batch := []*loggregator_v2.Envelope{
+			{
+				Timestamp:  time.Now().UnixNano(),
+				SourceId:   "jasonk",
+				InstanceId: "jason",
+				Tags: map[string]string{
+					"foo": "bar",
+				},
+				Message: &loggregator_v2.Envelope_Log{
+					Log: &loggregator_v2.Log{
+						Payload: []byte("warren is cool"),
+						Type:    loggregator_v2.Log_ERR,
+					},
+				},
+			},
+		}
+		log.Print("writing a batch")
+		ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+		_, err := client.Send(ctx, &logcache_v1.SendRequest{
+			LocalOnly: true,
+			Envelopes: &loggregator_v2.EnvelopeBatch{
+				Batch: batch,
+			},
+		})
+
+		if err != nil {
+			log.Printf("failed to write envelopes: %s", err)
+			continue
+		}
+		time.Sleep(time.Second)
+	}
+}
