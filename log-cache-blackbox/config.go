cd6686fabcb75af02b10a46fbcb74ba438ce1142
Johanna Smith <josmith@pivotal.io>
2018-05-02 13:20:02 -0600

    Update struct tags on config struct to report non secret values
    Signed-off-by: Brady Love <blove@pivotal.io>

--- log-cache-blackbox/config.go
+++ log-cache-blackbox/config.go
@@ -6,16 +6,16 @@ import (
 )
 
 type Config struct {
-	LogCacheURL *url.URL `env:"LOG_CACHE_URL,   required"`
+	LogCacheURL *url.URL `env:"LOG_CACHE_URL, required, report"`
 
-	Port    int     `env:"PORT,             required"`
-	VCapApp VCapApp `env:"VCAP_APPLICATION, required"`
+	Port    int     `env:"PORT,             required, report"`
+	VCapApp VCapApp `env:"VCAP_APPLICATION, required, report"`
 
-	UAAAddr         string `env:"UAA_ADDR,          required"`
+	UAAAddr         string `env:"UAA_ADDR,          required, report"`
 	UAAClient       string `env:"UAA_CLIENT,        required"`
-	UAAClientSecret string `env:"UAA_CLIENT_SECRET, required, noreport"`
+	UAAClientSecret string `env:"UAA_CLIENT_SECRET, required"`
 
-	SkipSSLValidation bool `env:"SKIP_SSL_VALIDATION"`
+	SkipSSLValidation bool `env:"SKIP_SSL_VALIDATION, report"`
 }
 
 type VCapApp struct {
