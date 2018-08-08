650a38c31d5b773ef15d983060fe312f1659f19c
Todd Persen <tpersen@pivotal.io>
2018-08-08 11:58:34 -0600

    Move log-cache-blackbox to the log-cache-tools repo
    Signed-off-by: Hunter Williams <huwilliams@pivotal.io>

--- log-cache-blackbox/reliability.go
+++
@@ -1,7 +0,0 @@
-package main
-
-type ReliabilityTestResult struct {
-	LogsSent        int `json:"logsSent"`
-	LogsReceived    int `json:"logsReceived"`
-	BadLogsReceived int `json:"badLogsReceived"`
-}
