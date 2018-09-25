3dada32f116a78ee80b0946dde0f62278f222589
Johannes Tuchscherer <jtuchscherer@pivotal.io>
2018-09-25 14:50:00 -0600

    allows to optionally omit the request logging in the post printer

--- postprinter/main.go
+++ postprinter/main.go
@@ -14,9 +14,14 @@ func main() {
 
 	port := os.Getenv("PORT")
 
+	var logRequests bool
+	logRequests = os.Getenv("SKIP_REQUEST_LOGGING") == "" || os.Getenv("SKIP_REQUEST_LOGGING") != "true"
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		defer r.Body.Close()
-		log.Printf("Request: %+v", r)
+		if logRequests {
+			log.Printf("Request: %+v", r)
+		}
 		data, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			log.Printf("Error reading body: %s", err)
