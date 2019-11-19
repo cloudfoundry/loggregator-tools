753dcb1fb720a036bf9af1ef7cc2f7cbad4ab0db
Max Eshleman <meshleman@pivotal.io>
2019-11-19 10:14:53 -0700

    add telegraf app

---
+++ telegraf/README.md
@@ -0,0 +1,5 @@
+# Usage
+1. Add output plugin(s) to telegraf.conf
+1. `cf` target the desired environment
+1. `credhub` target the desired environment
+1. `./push.sh`
---
+++ telegraf/push.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -eo pipefail
+
+telegraf_dir=$(cd $(dirname ${BASH_SOURCE}) && pwd)
+
+function create_security_group() {
+  echo "Creating Telegraf scrape security group"
+  cf create-security-group telegraf-scrape "${telegraf_dir}/asg.json"
+  cf bind-security-group telegraf-scrape system system
+}
+
+function download_telegraf() {
+  telegraf_version=$(curl -s https://api.github.com/repos/influxdata/telegraf/releases/latest | jq -r .tag_name)
+  telegraf_binary_url="https://dl.influxdata.com/telegraf/releases/telegraf-${telegraf_version}-static_linux_amd64.tar.gz"
+  wget -qO- "$telegraf_binary_url" | tar xvz - --strip=2 telegraf/telegraf
+}
+
+function create_certificates() {
+  mkdir -p certs
+  pushd certs > /dev/null
+    ca_cert_name=$(credhub find -n metric_scraper_ca --output-json | jq -r .credentials[].name | grep cf)
+    credhub generate -n telegraf_scrape_tls -t certificate --ca "$ca_cert_name" -c telegraf_scrape_tls
+
+    credhub get -n telegraf_scrape_tls --output-json | jq -r .value.ca > scrape_ca.crt
+    credhub get -n telegraf_scrape_tls --output-json | jq -r .value.certificate > scrape.crt
+    credhub get -n telegraf_scrape_tls --output-json | jq -r .value.private_key > scrape.key
+  popd > /dev/null
+}
+
+function push_telegraf() {
+  cf target -o system -s system
+  cf push telegraf -f "${telegraf_dir}/manifest.yml"
+}
+
+pushd ${telegraf_dir} > /dev/null
+  download_telegraf
+  create_security_group
+  create_certificates
+  push_telegraf
+popd > /dev/null
