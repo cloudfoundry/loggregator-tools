71cd3c6d2ef0cbed6e9bcac27e47a1d95ce003c7
Jesse Weaver <wjesse@vmware.com>
2020-08-04 16:41:20 +0000

    Add rate-limited log spinner

---
+++ lograter/go.mod
@@ -0,0 +1,5 @@
+module github.com/cloudfoundry-incubator/loggregator-tools/logspinner
+
+go 1.13
+
+require go.uber.org/ratelimit v0.1.0
---
+++ lograter/lograter.go
@@ -0,0 +1,65 @@
+// tool for generating log load
+
+// usage: curl <endpoint>?cycles=100&delay=1ms&text=time2
+// delay is duration format (https://golang.org/pkg/time/#ParseDuration)
+// defaults: 10 cycles, 1 second, "LogSpinner Log Message"
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+	"go.uber.org/ratelimit"
+)
+
+func main() {
+	http.HandleFunc("/", rootResponse)
+	fmt.Println("listening...")
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func rootResponse(res http.ResponseWriter, req *http.Request) {
+
+	rate, err := strconv.Atoi(req.FormValue("rate"))
+	if rate == 0 || err != nil {
+		rate = 100
+	}
+
+	duration, err := time.ParseDuration(req.FormValue("duration"))
+	if err != nil {
+		duration = time.Second
+	}
+
+	logText := (req.FormValue("text"))
+	if logText == "" {
+		logText = "LogSpinner Log Message"
+	}
+
+	go outputLog(rate, duration, logText)
+
+	fmt.Fprintf(res, "rate %d, duration %s, text %s\n", rate, duration, logText)
+}
+
+func outputLog(rate int, duration time.Duration, logText string) {
+	end := time.Now().Add(duration)
+	rl := ratelimit.New(rate)
+	now := time.Now()
+	total := 0
+	for time.Now().Before(end) {
+		total += 1
+		fmt.Printf("msg %d %s\n", total, logText)
+		rl.Take()
+	}
+	done := time.Now()
+	diff := done.Sub(now)
+
+	actualRate := float64(total) / diff.Seconds()
+	fmt.Printf("Duration %s TotalSent %d Rate %f \n", diff.String(), total, actualRate)
+
+}
