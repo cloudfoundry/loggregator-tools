edfbe6bad6948002b3042002e282770697d41505
Carson Long <12767276+ctlong@users.noreply.github.com>
2025-04-29 10:30:22 -0700

    Bump slow_consumer dependencies

--- slow_consumer/go.mod
+++ slow_consumer/go.mod
@@ -1,21 +1,19 @@
 module code.cloudfoundry.org/loggregator-tools/slow_consumer
 
-go 1.21
+go 1.24
 
-require github.com/cloudfoundry/noaa v2.1.0+incompatible
+require github.com/cloudfoundry/noaa/v2 v2.5.0
 
 require (
-	github.com/apoydence/eachers v0.0.0-20181020210610-23942921fe77 // indirect
-	github.com/cloudfoundry/sonde-go v0.0.0-20220627221915-ff36de9c3435 // indirect
-	github.com/elazarl/goproxy v0.0.0-20221015165544-a0805db90819 // indirect
+	github.com/cloudfoundry/sonde-go v0.0.0-20230710164515-a0a43d1dbbf8 // indirect
 	github.com/elazarl/goproxy/ext v0.0.0-20221015165544-a0805db90819 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
-	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/google/go-cmp v0.5.9 // indirect
-	github.com/gorilla/websocket v1.5.0 // indirect
-	github.com/josharian/intern v1.0.0 // indirect
-	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/nxadm/tail v1.4.8 // indirect
 	github.com/onsi/ginkgo v1.16.5 // indirect
-	github.com/onsi/gomega v1.24.1 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/tools v0.24.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 )
---
+++ slow_consumer/vendor/github.com/cloudfoundry/noaa/v2/consumer/sync.go
@@ -0,0 +1,149 @@
+package consumer
+
+import (
+	"bytes"
+	"fmt"
+	"mime/multipart"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/cloudfoundry/noaa/v2"
+	"github.com/cloudfoundry/sonde-go/events"
+	"google.golang.org/protobuf/proto"
+)
+
+// ContainerMetrics is deprecated in favor of ContainerEnvelopes, since
+// returning the ContainerMetric type directly hides important
+// information, like the timestamp.
+//
+// The returned values will be the same as ContainerEnvelopes, just with
+// the Envelope stripped out.
+func (c *Consumer) ContainerMetrics(appGuid string, authToken string) ([]*events.ContainerMetric, error) {
+	envelopes, err := c.ContainerEnvelopes(appGuid, authToken)
+	if err != nil {
+		return nil, err
+	}
+	messages := make([]*events.ContainerMetric, 0, len(envelopes))
+	for _, env := range envelopes {
+		messages = append(messages, env.GetContainerMetric())
+	}
+	noaa.SortContainerMetrics(messages)
+	return messages, nil
+}
+
+// ContainerEnvelopes connects to trafficcontroller via its 'containermetrics'
+// http(s) endpoint and returns the most recent dropsonde envelopes for an app.
+func (c *Consumer) ContainerEnvelopes(appGuid, authToken string) ([]*events.Envelope, error) {
+	envelopes, err := c.readTC(appGuid, authToken, "containermetrics")
+	if err != nil {
+		return nil, err
+	}
+	for _, env := range envelopes {
+		if env.GetEventType() == events.Envelope_LogMessage {
+			return nil, fmt.Errorf("Upstream error: %s", env.GetLogMessage().GetMessage())
+		}
+	}
+	return envelopes, nil
+}
+
+func (c *Consumer) readTC(appGuid string, authToken string, endpoint string) ([]*events.Envelope, error) {
+	trafficControllerUrl, err := url.ParseRequestURI(c.trafficControllerUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	recentPath := c.recentPathBuilder(trafficControllerUrl, appGuid, endpoint)
+
+	resp, err := c.requestTC(recentPath, authToken)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	reader, err := getMultipartReader(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	var buffer bytes.Buffer
+
+	var envelopes []*events.Envelope
+	for part, loopErr := reader.NextPart(); loopErr == nil; part, loopErr = reader.NextPart() {
+		buffer.Reset()
+
+		_, err = buffer.ReadFrom(part)
+		if err != nil {
+			break
+		}
+
+		envelope := new(events.Envelope)
+		if err := proto.Unmarshal(buffer.Bytes(), envelope); err != nil {
+			continue
+		}
+
+		envelopes = append(envelopes, envelope)
+	}
+
+	return envelopes, nil
+}
+
+func (c *Consumer) requestTC(path, authToken string) (*http.Response, error) {
+	if authToken == "" && c.refreshTokens {
+		return c.requestTCNewToken(path)
+	}
+	var err error
+	resp, httpErr := c.tryTCConnection(path, authToken)
+	if httpErr != nil {
+		err = httpErr.error
+		if httpErr.statusCode == http.StatusUnauthorized && c.refreshTokens {
+			resp, err = c.requestTCNewToken(path)
+		}
+	}
+	return resp, err
+}
+
+func (c *Consumer) requestTCNewToken(path string) (*http.Response, error) {
+	token, err := c.getToken()
+	if err != nil {
+		return nil, err
+	}
+	conn, httpErr := c.tryTCConnection(path, token)
+	if httpErr != nil {
+		return nil, httpErr.error
+	}
+	return conn, nil
+}
+
+func (c *Consumer) tryTCConnection(recentPath, token string) (*http.Response, *httpError) {
+	req, _ := http.NewRequest("GET", recentPath, nil)
+	req.Header.Set("Authorization", token)
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		message := `Error dialing trafficcontroller server: %s.
+Please ask your Cloud Foundry Operator to check the platform configuration (trafficcontroller endpoint is %s).`
+		return nil, &httpError{
+			statusCode: -1,
+			error:      fmt.Errorf(message, err, c.trafficControllerUrl),
+		}
+	}
+
+	return resp, checkForErrors(resp)
+}
+
+func getMultipartReader(resp *http.Response) (*multipart.Reader, error) {
+	contentType := resp.Header.Get("Content-Type")
+
+	if len(strings.TrimSpace(contentType)) == 0 {
+		return nil, ErrBadResponse
+	}
+
+	matches := boundaryRegexp.FindStringSubmatch(contentType)
+
+	if len(matches) != 2 || len(strings.TrimSpace(matches[1])) == 0 {
+		return nil, ErrBadResponse
+	}
+	reader := multipart.NewReader(resp.Body, matches[1])
+	return reader, nil
+}
