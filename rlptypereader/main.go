d834373ef7dccbf63332e09f2ae36fde3147c4a3
Carson Long <12767276+ctlong@users.noreply.github.com>
2025-04-29 10:36:37 -0700

    Bump rlptypereader dependencies

--- rlptypereader/main.go
+++ rlptypereader/main.go
@@ -10,7 +10,7 @@ import (
 
 	"google.golang.org/grpc"
 
-	"code.cloudfoundry.org/go-loggregator/rpc/loggregator_v2"
+	"code.cloudfoundry.org/go-loggregator/v10/rpc/loggregator_v2"
 	"code.cloudfoundry.org/loggregator-release/src/plumbing"
 )
 
